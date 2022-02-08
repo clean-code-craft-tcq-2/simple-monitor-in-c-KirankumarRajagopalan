8df5424bd54cdd2b07443d521dceea69de6d5f9d
KirankumarRajagopalan <91051522+KirankumarRajagopalan@users.noreply.github.com>
2022-02-08 23:57:54 +0530

    Update CheckBatteryStatus.c

--- CheckBatteryStatus.c
+++ CheckBatteryStatus.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <assert.h>
+#include "CheckBatteryStatus.h"
 
+int batteryIsOk(float temperature, float soc, float chargeRate) {
+  if(temperature < 0 || temperature > 45) {
+    printf("Temperature out of range!\n");
+    return 0;
+  } else if(soc < 20 || soc > 80) {
+    printf("State of Charge out of range!\n");
+    return 0;
+  } else if(chargeRate > 0.8) {
+    printf("Charge Rate out of range!\n");
+    return 0;
+  }
+  return 1;
+}
