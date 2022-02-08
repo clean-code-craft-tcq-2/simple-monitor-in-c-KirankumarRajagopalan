624545b6ba8aab0d17478e03674b7e18c2710380
KirankumarRajagopalan <91051522+KirankumarRajagopalan@users.noreply.github.com>
2022-02-09 00:57:56 +0530

    Update CheckBatteryStatus.c

--- CheckBatteryStatus.c
+++ CheckBatteryStatus.c
@@ -2,16 +2,36 @@
 #include <assert.h>
 #include "CheckBatteryStatus.h"
 
-int batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
-    printf("Temperature out of range!\n");
-    return 0;
-  } else if(soc < 20 || soc > 80) {
-    printf("State of Charge out of range!\n");
-    return 0;
-  } else if(chargeRate > 0.8) {
-    printf("Charge Rate out of range!\n");
-    return 0;
+int checkIfItIsInSpecifiedRange(float givenValue, float lowerLimit, float upperLimit){
+  if(givenValue < lowerLimit || temperature > upperLimit) {
+        return 0;
   }
   return 1;
+}
+
+int checkIfItIsInRange(float givenValue, float threshold){
+  if(givenValue > threshold){
+        return 0;
+  }
+  return 1;
+}
+
+int checkBatteryTemperature(float temp){
+  int status = checkIfItIsInSpecifiedRange(temp, LOWER_TEMPERATURE_LIMIT, HIGHER_TEMPERATURE_LIMIT);
+  return status;
+}
+
+int checkSoCRange(float soc){
+    int status = checkIfItIsInSpecifiedRange(soc, LOWER_SOC_LIMIT, HIGHER_SOC_LIMIT);
+  return status;
+}
+
+int checkChargeRate(float chargeRate){
+    int status = checkIfItIsInRange(chargeRate, CHARGERATE_THRESHOLD);
+  return status;
+}
+
+int checkBatteryHealth(float temp, float soc, float chargeRate){
+    int batteryStatus = (checkBatteryTemperature(temp)) + (checkSoCRange(soc)) + (checkChargeRate(chargeRate));
+  return batteryStatus;
 }
